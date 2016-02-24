6f95dcea0bceed45175bfc1211d5eb7a632cfc05
sue445 <sue445@sue445.net>
2016-02-24 22:54:40 +0900

    bundle gem itamae-plugin-recipe-tmux --mit

---
+++ lib/itamae/plugin/recipe/tmux.rb
@@ -0,0 +1,11 @@
+require "itamae/plugin/recipe/tmux/version"
+
+module Itamae
+  module Plugin
+    module Recipe
+      module Tmux
+        # Your code goes here...
+      end
+    end
+  end
+end
