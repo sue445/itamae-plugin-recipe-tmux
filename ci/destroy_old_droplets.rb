c6788ff71adedf8f05823abd81c4ab0e88f35ccf
sue445 <sue445@sue445.net>
2016-03-07 01:40:43 +0900

    Fix token name

--- ci/destroy_old_droplets.rb
+++ ci/destroy_old_droplets.rb
@@ -7,7 +7,7 @@ http = Net::HTTP.new("api.digitalocean.com", 443)
 http.use_ssl = true
 
 res = http.start do
-  http.get("/v2/droplets", "Authorization" => "Bearer #{ENV['DIGITALOCEAN_TOKEN']}")
+  http.get("/v2/droplets", "Authorization" => "Bearer #{ENV['DIGITALOCEAN_ACCESS_TOKEN']}")
 end
 
 droplets = JSON.parse(res.body)['droplets']
@@ -16,7 +16,7 @@ droplets.each do |droplet|
   if Time.now - Time.parse(droplet['created_at']) >= 60 * 60
     puts "destroying #{droplet}..."
     res = http.start do
-      http.delete("/v2/droplets/#{droplet['id']}", "Authorization" => "Bearer #{ENV['DIGITALOCEAN_TOKEN']}")
+      http.delete("/v2/droplets/#{droplet['id']}", "Authorization" => "Bearer #{ENV['DIGITALOCEAN_ACCESS_TOKEN']}")
     end
   end
 end
